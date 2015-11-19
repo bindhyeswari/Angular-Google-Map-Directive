123c0e6708976e39ebcbfff055c867a0e297b2cc
BP Mishra <tech@golivelabs.io>
2015-11-19 13:06:16 -0800

    first draft

---
+++ utilities/google-map-directive.js
@@ -0,0 +1,13 @@
+/**
+ * Created by bindhyeswarimishra on 11/19/15.
+ */
+
+// add the script to the directive map in the compile phase
+
+var utilitiesApp = angular.module('gllUtilities', []);
+
+utilitiesApp.directive('map', function () {
+    return {
+
+    };
+});
